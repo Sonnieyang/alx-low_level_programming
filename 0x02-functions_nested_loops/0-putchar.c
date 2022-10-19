4f23ddec3d2fae86bac8f708cfa2778975d1d633
Sonnieyang <egbuasoni@gmail.com>
2022-10-18 21:09:21 -0700

    nested loops task zero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
-#include "putchar.h"
+#include "main.h"
 
 /**
- * main - prints putchar
- *
- * Return: 0
+ * main - prints putchar and a new line
+ * Return: Always 0 (success)
  */
-
 int main(void)
 {
-	char text[8] = "putchar";
-	int i = 0;
-
-	for (i = 0; i < 7; i++)
-	{
-		_putchar(text[i]);
-	}
-	_putchar('\n');
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 
 	return (0);
 }
