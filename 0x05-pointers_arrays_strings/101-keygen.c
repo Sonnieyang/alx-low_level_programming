f0e18a833df5149a7825f971ab50f1eefc11f341
Sonnieyang <egbuasoni@gmail.com>
2022-10-25 20:38:45 -0700

    Pointers_Arrays_and_Strings task ten

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generate random valid password
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int pass{
+		[100];
+		int i, sum, n;
+
+		sum = 0
+
+			srand(time(NULL));
+		for 
