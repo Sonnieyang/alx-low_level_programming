6a6c83fc83816f561ae2e9b3d8422ae6f3342eb2
Sonnieyang <egbuasoni@gmail.com>
2022-11-08 20:45:51 -0800

    hot fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -39,3 +39,27 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 }
+
+/**
+ * is_number - checks to see if input is a number
+ * @s: input to check for numberhood
+ * Return: 1 if it is a number, 0 if not
+ */
+int is_number(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= '0' && *(s + i) <= '9')
+		{
+			i++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
