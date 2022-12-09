c5eca8c910bb1303592dbd55b9579ce3abf98708
Sonnieyang <egbuasoni@gmail.com>
2022-12-09 12:17:32 -0800

    task three hot fix

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -27,25 +27,38 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 	char *string;
-	int length;
+	list_t *temp;
 
+	temp = *head;
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-	if (str == NULL)
-	{
-		string = NULL;
-		length = 0;
-	}
-	else
-	{
-		string = strdup(str);
-		length = _strlen(str);
-	}
-	new->len = length;
-	new->str = string;
-	new->next = *head;
-	*head = new;
 
-	return (*head);
+	if (str == NULL)
+		return (NULL);
+
+	string = strdup(str);
+	if (string == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->len = _strlen(str);
+	new->str = string;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = new;
+
+	return (new);
 }
